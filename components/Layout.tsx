b864ec5184f07ad0b628098de9634fc9ff51fdfa
Yuto Mabe <yuto96315mabe@gmail.com>
2023-02-20 21:26:24 +0900

    表示テストとtailwindの設定

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode } from "react";
+import Head from "next/head";
+import { CheckBadgeIcon } from '@heroicons/react/24/solid';
+
+type Title = {
+    title: string;
+    children: ReactNode;
+};
+
+export const Layout: React.FC<Title> = ({ children, title = "Todo app" }) => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center font-mono text-blue-500">
+            <Head>{title}</Head>
+            <header></header>
+            <main className="flex w-screen flex-1 flex-col items-center justify-center">
+                {children}
+            </main>
+            <footer className="flex h-12 w-full items-center justify-center border-t">
+                <CheckBadgeIcon className="h-6 w-6 text-blue-500" />
+            </footer>
+        </div>
+    );
+};
